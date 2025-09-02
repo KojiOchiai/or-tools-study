3d46c57754dc3b33fb508285d381e83fb383eb03
KojiOchiai <12869331+KojiOchiai@users.noreply.github.com>
2025-09-02 22:28:39 +0900

    feat: implement task with wait

--- hello.py
+++ hello.py
@@ -1,38 +1,65 @@
+from dataclasses import dataclass
+
 from ortools.sat.python import cp_model
 
 model = cp_model.CpModel()
 
+
+@dataclass
+class Task:
+    name: str
+    duration1: int
+    wait: int
+    duration2: int
+    start1: cp_model.IntVar | None = None
+    end1: cp_model.IntVar | None = None
+    start2: cp_model.IntVar | None = None
+    end2: cp_model.IntVar | None = None
+    interval1: cp_model.IntervalVar | None = None
+    interval2: cp_model.IntervalVar | None = None
+
+
 # 各タスク
-tasks = {
-    "job1": {"duration": 3, "consumption": 2},
-    "job2": {"duration": 2, "consumption": 1},
-    "job3": {"duration": 4, "consumption": 3},
-}
+tasks = [
+    Task("A", 3, 2, 2),
+    Task("B", 2, 5, 4),
+    Task("C", 1, 3, 1),
+]
 
 
-# タスク開始時間変数
-start_ver = []
-end_ver = []
-intervals = []
-
-for name, info in tasks.items():
+max_time = 100
+for task in tasks:
     # タスクの開始時刻、終了時刻
-    start = model.NewIntVar(0, 10, f"start_{name}")
-    end = model.NewIntVar(0, 10, f"end_{name}")
-    model.Add(end == start + info["duration"])
+    start1 = model.NewIntVar(0, max_time, f"start1_{task.name}")
+    end1 = model.NewIntVar(0, max_time, f"end1_{task.name}")
+    model.Add(end1 == start1 + task.duration1)
+    start2 = model.NewIntVar(0, max_time, f"start2_{task.name}")
+    end2 = model.NewIntVar(0, max_time, f"end2_{task.name}")
+    model.Add(end2 == start2 + task.duration2)
+    model.Add(start2 == end1 + task.wait)
     # リソース競合、タスクが重ならないようにする
-    interval = model.NewIntervalVar(start, info["duration"], end, f"interval_{name}")
-    start_ver.append(start)
-    end_ver.append(end)
-    intervals.append(interval)
+    interval1 = model.NewIntervalVar(
+        start1, task.duration1, end1, f"interval1_{task.name}"
+    )
+    interval2 = model.NewIntervalVar(
+        start2, task.duration2, end2, f"interval2_{task.name}"
+    )
+    task.start1 = start1
+    task.end1 = end1
+    task.start2 = start2
+    task.end2 = end2
+    task.interval1 = interval1
+    task.interval2 = interval2
 
 # リソース競合
+intervals_ = [task.interval1 for task in tasks] + [task.interval2 for task in tasks]
+intervals = [interval for interval in intervals_ if interval is not None]
 model.AddNoOverlap(intervals)
 
 # 目的関数: makespan 最小化
 makespan = model.NewIntVar(0, 20, "makespan")
-for end in end_ver:
-    model.Add(end <= makespan)
+for task in tasks:
+    model.Add(task.end2 <= makespan)
 model.Minimize(makespan)
 
 solver = cp_model.CpSolver()
@@ -40,7 +67,16 @@ status = solver.Solve(model)
 
 if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
     print("Objective value =", solver.ObjectiveValue())
-    for start, end in zip(start_ver, end_ver):
-        print(f"start_{start}: {solver.Value(start)} - {solver.Value(end)}")
+    for task in tasks:
+        if (
+            task.start1 is not None
+            and task.end1 is not None
+            and task.start2 is not None
+            and task.end2 is not None
+        ):
+            print(
+                f"{task.name}: {solver.Value(task.start1)} - {solver.Value(task.end1)}"
+                f", {solver.Value(task.start2)} - {solver.Value(task.end2)}"
+            )
 else:
     print("No solution found.")
